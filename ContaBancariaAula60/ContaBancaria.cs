11fe15947920e618cb918e3a0cbb2ef7300256e1
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-06-09 16:15:00 -0300

    Exercicio com tecnica de POO

---
+++ ContaBancariaAula60/ContaBancaria.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancariaAula60
+{
+    class ContaBancaria
+    {
+        // Propriedades autoimplementadas:                 
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        // Criacao dos construtores com dois argumentos:
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;            
+        }
+
+        // Construtor, reaproitando os atributo acima, atraves da palavra this
+        public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular) // reaproitando o contrutuor de dois argumentos acima (numero e titular)
+        {
+            Deposito(depositoInicial);
+        }
+
+        // Metodos da Classe:
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0;
+        }        
+        // Criando o ToString:
+        public override string ToString()
+        {
+            return "Conta: "
+                   + Numero
+                   + ", Titular: "
+                   + Titular
+                   + ", Saldo: $"
+                   + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ContaBancariaAula60/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancariaAula60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declarando a variavel conta:
+            ContaBancaria conta;
+
+            //ContaBancaria c = new ContaBancaria();
+
+            Console.Write("Entre com o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            
+            Console.Write("Haverá deposito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Entre com o valor do depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else conta = new ContaBancaria(numero, titular);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados da Conta: ");
+            Console.WriteLine(conta);
+            Console.WriteLine();
+
+            
+            // Imprimir na tela deposito:
+            Console.Write("Entre com um valor para depósito: ");           
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(deposito);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados da conta atualizados");
+            Console.WriteLine(conta);
+            Console.WriteLine();
+
+            // Imprimir na tela saque:
+            Console.Write("Entre com um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(saque);
+            Console.WriteLine("Dados da conta atualizados");
+            Console.WriteLine(conta);
+            
+        }
+    }
+}
